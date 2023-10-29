3de181cbcaa33492d3b0f6e61cc90c24f2a08a3e
cool-serg <cool-serg@mail.ru>
2023-10-29 18:14:53 +0300

    Выполнение задания

--- game_v2.py
+++ game_v2.py
@@ -41,10 +41,36 @@ def score_game(random_predict) -> int:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+  
+    count = 0
+    
+    a = 1
+    b = 101
+    predict = np.random.randint(a, b)
+
+    while number != predict:
+        count += 1
+        predict = (a+b)//2
+        if number > predict:
+            a = predict + 1
+        elif number < predict:
+            b = predict - 1
+
+    return count
 
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
+    score_game(game_core_v3)
